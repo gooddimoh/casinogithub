d252ddcdfc2082e395908607a601eba4c611cdb3
gooddimoh <gooddimoh@gmail.com>
2022-05-31 19:23:01 +0300

    first commit

---
+++ app/Http/Controllers/CasinoDashboardController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CasinoDashboardController extends Controller
+{
+    public function index(){
+
+    }
+    
+    
+    public function winlose(){
+        
+    }
+
+
+    public function ()
+    {
+        
+    }
+}
---
+++ resources/parts/form.blade.php
@@ -0,0 +1,4 @@
+<form action="" method="post">
+    <input type="text" name="username">
+    <input type="text" name="phonenumber">
+</form>
